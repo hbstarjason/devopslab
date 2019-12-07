8125ab02f82ef21f811602b2ac5bc69d4d0103d3
hbstarjason <hbstarjason@gmail.com>
2019-12-07 14:31:57 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
-export GITACCOUNT=
+export GITACCOUNT=hbstarjason
 
-export MYCLUSTER=
-export KUBECONFIG=
+export MYCLUSTER=tektonknativebeijing69
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing69/kube-config-tok04-tektonknativebeijing69.yml
 
-export NAMESPACE=
-export EMAIL=
-export INGRESS=
+export NAMESPACE=tektondevops-zhang
+export EMAIL=1124484297@qq.com
+export INGRESS=tektonknativebeijing69-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud 
